b3eaf5f4130fe71dc2437ac9dcf869f2d61374a6
08ming <754041231@qq.com>
2020-06-03 17:31:07 +0800

    Add Start

---
+++ src/main/java/com/Ka1/ApplicationStart.java
@@ -0,0 +1,13 @@
+package com.Ka1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStart.class, args);
+	}
+
+}
